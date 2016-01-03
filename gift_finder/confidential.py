66ad7ab4fb1f64a4c1b0d490eedbf9d0f3c010a0
Austin Malerba <austin@Austins-MacBook-Pro.local>
2016-01-03 18:44:08 -0500

    Second commit

--- gift_finder/confidential.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'austin'
-
-
-SECRET_KEY = 'pwdr--x9_uea7=pei#_(wv^r^&z&l$!pl9h^&ktmh3ve%-)^p('
-EMAIL_HOST_PASSWORD = 'rowber118'
-AWS_SECRET_ACCESS_KEY="mIYZadnzK/hJ0J94L3+kS2Kj7DyMuavvScA2CIDj"
